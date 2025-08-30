41fc39edbb104672119b9b8ccf38158fc696d1f0
LUCAS M MORGAN <enviraldesign@gmail.com>
2025-08-30 17:16:14 -0400

    Revamp landing page

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ yearEl.textContent = new Date().getFullYear();
 
 const navToggle = document.getElementById('nav-toggle');
 const navMenu = document.getElementById('nav-menu');
+const themeToggle = document.getElementById('theme-toggle');
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('open');
+});
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('light');
 });
